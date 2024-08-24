05b976ec92fc90097eadb148b4bf6a6dd513f0ff
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-08-24 11:43:43 +0300

    ADD DbContext

--- ShopEntityFramework/Program.cs
+++ ShopEntityFramework/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ShopEntityFramework
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopEntityFramework
 {
     class Position
     {
@@ -28,6 +30,16 @@
     class Category
     {
 
+    }
+    class ShopDbContext : DbContext
+    {
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Shop> Shops { get; set; }
     }
     internal class Program
     {
