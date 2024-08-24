53ab5448e46042a4625b707d54e4f4670e414945
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2024-08-24 11:40:02 +0300

    Add project files.

---
+++ ShopEntityFramework/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ShopEntityFramework
+{
+    class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    class Worker
+    {
+
+    }
+    class Shop
+    {
+
+    }
+    class City
+    {
+
+    }
+    class Country
+    {
+
+    }
+    class Product
+    {
+
+    }
+    class Category
+    {
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
